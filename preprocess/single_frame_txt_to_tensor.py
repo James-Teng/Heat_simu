7f85af22d247a5d4fef94463de4fe2dce2914b80
Tessra <763851927@qq.com>
2023-09-18 11:34:36 +0800

    完善注释，优化结构

--- preprocess/single_frame_txt_to_tensor.py
+++ preprocess/single_frame_txt_to_tensor.py
@@ -1,24 +1,17 @@
 import os
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 
 
-# to do：解决数据中 nan 的问题，暂时换成0
-
-def convert_frame(data, index):
-    """
-    读取文件，并转换指定的一帧
-    :param data: 处理成
-    :param index:
-    :return:
-    """
-    pass
-
+# to do：解决数据中 nan 的问题，暂时换成 0
 
 data_path = r'E:\Research\Project\Heat_simu\data\data2_even\txt_format\0.1K_0.1gap.txt'
 output_path = r'E:\Research\Project\Heat_simu\data\data2_even\tensor_format'
 
-frame = 130
+frame = 190  # index from 0 to max - 1
+max_T = 400  # 摄氏度
+min_T = 0
 
 if __name__ == '__main__':
 
@@ -31,27 +24,41 @@ if __name__ == '__main__':
             print(line)
 
         # 处理时间信息，未完成,以下三行丢掉了时间信息
-        line = f.readline().strip('\n')  # 读取一行，并去掉换行符
+        line = f.readline().strip('\n')
         line_split = line.split()  # 按照空格进行分割
         print(line[1:3])
 
-        # 读取点分布
+        # 读取采样点分布与一帧
         print('\n{:-^52}\n'.format(' One Frame '))
         points = []
         data = []
         line = f.readline().strip('\n')
+        run_once = True
+
         while line:
+
+            # 分割数据
             line_split = line.split()  # 按照空格进行分割
-            points.append([float(x) for x in line_split[:2]])  # 读取坐标信息，字符串转数字
-            # 将一帧的数据从字符串转换为数字
+
+            # 统计帧总数
+            if run_once:
+                print(f'total frame: {len(line_split) - 2}')
+                run_once = False
+
+            # 读取采样点坐标信息
+            points.append([float(x) for x in line_split[:2]])  # 前两位为坐标，格式为字符串，需要转浮点数
+
+            # 读取某一帧的数据，并替换 NaN
             if line_split[frame+2] != 'NaN':
                 data.append(float(line_split[frame+2]))  # 读取第一帧的数据，统计最大最小，验证将nan变成0是否合理，之后可能要考虑一下边界的问题
             else:
                 data.append(float(0))  # 此处简单赋值为 0，需要验证合理
-            # 此处还需要生成一个mask
+
+            # 训练时此处还需要生成一个mask
+
             line = f.readline().strip('\n')
 
-        # 统计矩阵大小
+        # 统计单帧张量大小
         column = 0
         row = 1
         r_former = points[0]
@@ -63,7 +70,7 @@ if __name__ == '__main__':
                 column = 1
                 row += 1
                 r_former = points[0]
-        print(f'row:{row}, column:{column}')  # 横向(r) 130 纵向(z) 260
+        print(f'tensor size: row - {row}, column - {column}')  # 横向(r) 130 纵向(z) 260
 
         # 转换一帧
         # frame = np.zeros((row, column))
@@ -75,7 +82,7 @@ if __name__ == '__main__':
         # visualization
         print(f'the {frame}th frame')
         plt.figure()
-        plt.imshow(distrib)
+        plt.imshow(distrib, vmin=min_T, vmax=max_T, cmap='jet')  # 建立颜色映射
         plt.axis('off')
         plt.show()
 
