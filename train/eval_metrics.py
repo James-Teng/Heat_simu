73ed852e5fc4e698a18b0cb6adfbdb8f1bf21d00
Tessra <763851927@qq.com>
2023-10-19 15:44:02 +0800

    add eval metrics

--- train/eval_metrics.py
+++ train/eval_metrics.py
@@ -1,1 +1,20 @@
 # todo psnr ssim
+
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
+
+
+def psnr(image_true, image_test, data_range=2.):
+    return peak_signal_noise_ratio(
+        image_true.cpu().numpy(),
+        image_test.cpu().numpy(),
+        data_range=data_range,
+    )
+
+
+def ssim(image_true, image_test, data_range=2.):
+    return structural_similarity(
+        image_true.cpu().numpy(),
+        image_test.cpu().numpy(),
+        data_range=data_range
+    )
+
